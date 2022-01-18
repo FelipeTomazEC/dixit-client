85f4e478cf68b1b7588a7b8c007f2e1330aa3176
FelipeTomazEC <felipetomaz2206@gmail.com>
2022-01-18 15:23:11 -0300

    feat: add enter a room and start game buttons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import { AvatarSelector } from '../components/AvatarSelector'
+import { Button } from '../components/Button';
 import { Layout } from '../components/Layout'
 import { TextInput } from '../components/TextInput';
 
 export default function Home() {
   const [avatar, setAvatar] = useState<string>("https://images.vexels.com/media/users/3/134485/isolated/preview/bcde859a8ad3a45cb93aed78d8a63686-emoticon-legal-emoji.png");
   const [username, setUsername] = useState<string>('');
+  const handleEnterRoom = () => alert('Entering a room...');
+  const handleStartNewGame = () => alert('Starting a new game...');
 
   return (
     <Layout>
@@ -17,6 +20,8 @@ export default function Home() {
         label='Enter a username' 
         onChange={(value) => setUsername(value)}
       />
+      <Button text='Enter a room' onClick={handleEnterRoom} isOutlined/>
+      <Button text='Start new game' onClick={handleStartNewGame}/>
     </Layout>
   );
 }
