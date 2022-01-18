e9ae55960a1638d470dd4bb0eb345bc022177e5f
FelipeTomazEC <felipetomaz2206@gmail.com>
2022-01-18 09:04:04 -0300

    feat: add username input

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
-import styled from 'styled-components'
-import AvatarSelector from '../components/AvatarSelector'
-import Layout from '../components/Layout'
+import { AvatarSelector } from '../components/AvatarSelector'
+import { Layout } from '../components/Layout'
+import { TextInput } from '../components/TextInput';
 
 export default function Home() {
   const [avatar, setAvatar] = useState<string>("https://images.vexels.com/media/users/3/134485/isolated/preview/bcde859a8ad3a45cb93aed78d8a63686-emoticon-legal-emoji.png");
+  const [username, setUsername] = useState<string>('');
 
   return (
     <Layout>
@@ -12,6 +13,10 @@ export default function Home() {
         avatar={avatar}
         onSelect={(newAvatar) => setAvatar(newAvatar)}
       />
+      <TextInput 
+        label='Enter a username' 
+        onChange={(value) => setUsername(value)}
+      />
     </Layout>
-  )
+  );
 }
