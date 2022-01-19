26c1fb29e1c05ca52b18280f3642440d52470e36
FelipeTomazEC <felipetomaz2206@gmail.com>
2022-01-18 22:33:31 -0300

    feat: implement dummy navigation to lobby page

--- templates/EnterRoom/index.tsx
+++ templates/EnterRoom/index.tsx
@@ -3,10 +3,19 @@ import { Button } from '../../components/Button';
 import { Layout } from '../../components/Layout';
 import { TextInput } from '../../components/TextInput';
 import { Container } from './styles';
+import { useRouter } from 'next/router';
 
 export const EnterRoom: React.FC = () => {
   const [roomCode, setRoomCode] = useState<string>('');
-  const handleEnterClick = () => alert(`Entering room ${roomCode}.`);
+  const router = useRouter();
+
+  const handleEnterClick = () => {
+    alert(`Entering room ${roomCode}.`);
+    
+    setTimeout(() => {
+      router.push('/lobby');
+    }, 3000);
+  }
 
   return (
     <Layout>
