9fe34b04f4c053e8aa7c0ce7beb5b223428f4a5b
FelipeTomazEC <felipetomaz2206@gmail.com>
2022-01-20 21:50:23 -0300

    feat: create choose picture page

---
+++ pages/choose-picture.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { ChoosePicture } from '../templates/ChoosePicture';
+
+const ChoosePicturePage: React.FC = () => {
+  return <ChoosePicture />;
+}
+
+export default ChoosePicturePage;
