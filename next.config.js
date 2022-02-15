c8502ffc0abd6172f7c8e5ea40544f25de98ccbd
FelipeTomazEC <felipetomaz2206@gmail.com>
2022-02-15 19:51:45 -0300

    fix: trim servers name

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   images: {
-    domains: process.env.IMAGE_SERVERS.split(',')
+    domains: process.env.IMAGE_SERVERS.split(',').map((server) => server.trim())
   },
   env: {
     WS_URL: process.env.WS_URL,
